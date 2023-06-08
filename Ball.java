edd233126fcca1ae51ef624b50a9149ec0637c78
Matthew <105758744+matthew-james-brewer@users.noreply.github.com>
2023-06-08 10:44:58 -0500

    Update Ball.java/getMessage()

--- Ball.java
+++ Ball.java
@@ -4,6 +4,6 @@ public class Ball extends Throwable {
 	public Ball(int radius) {this.radius = radius;}
 	public int getRadius() {return this.radius;}
 	public String getMessage() {
-	return "Ball of radius "+radius+" thrown, and the compiler got knocked out.";
+	return "The compiler got knocked out because Ball of radius "+radius+" thrown and hit compiler.";
 	}
 }
