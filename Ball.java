412256f7938934992dff8b1f96c1e8347e89e748
Matthew <105758744+matthew-james-brewer@users.noreply.github.com>
2023-06-08 10:49:37 -0500

    Update Ball.java with spacing

--- Ball.java
+++ Ball.java
@@ -1,9 +1,9 @@
 public class Ball extends Throwable {
 	private int radius;
-	public Ball() {this.radius = 1;}
-	public Ball(int radius) {this.radius = radius;}
-	public int getRadius() {return this.radius;}
+	public Ball() { this.radius = 1; }
+	public Ball(int radius) { this.radius = radius; }
+	public int getRadius() { return this.radius; }
 	public String getMessage() {
-	return "The compiler got knocked out because Ball of radius "+radius+" thrown and hit compiler.";
+		return "The compiler got knocked out because Ball of radius "+radius+" thrown and hit compiler.";
 	}
 }
