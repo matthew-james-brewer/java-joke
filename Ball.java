56ca25f4c22db36bdcc9a111c1c198c75dbaf2f6
Matthew <105758744+matthew-james-brewer@users.noreply.github.com>
2023-06-08 10:56:11 -0500

    Update Ball.java with negative number handling

--- Ball.java
+++ Ball.java
@@ -1,8 +1,17 @@
 public class Ball extends Throwable {
 	private int radius;
-	public Ball() { this.radius = 1; }
-	public Ball(int radius) { this.radius = radius; }
-	public int getRadius() { return this.radius; }
+	public Ball() {
+		this.radius = 1;
+	}
+	public Ball(int radius) throws NegativeNumberException {
+		if ( radius < 0 ) {
+			throw new NegativeNumberException("Negative radius not allowed: " + radius);
+		}
+		this.radius = radius;
+	}
+	public int getRadius() {
+		return this.radius;
+	}
 	public String getMessage() {
 		return "The compiler got knocked out because Ball of radius "+radius+" thrown and hit compiler.";
 	}
